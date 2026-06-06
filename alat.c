bfc8a9d0c1a081de048e15790e2bff84c9e25034
auzan-valy-hafidza <auzanvaly@mhs.usk.ac.id>
2026-06-06 23:24:54 +0700

    Update alat.c
    cicilan kpr

--- alat.c
+++ alat.c
@@ -32,3 +32,158 @@ void swapData(Alat *a, Alat *b)
     a->harga = b->harga;
     b->harga = tempHarga;
 }
+
+void tambahAlat()
+{
+    Alat *baru =
+    (Alat*)malloc(sizeof(Alat));
+
+    printf("ID Alat : ");
+    scanf("%d",&baru->id);
+
+    printf("Nama Alat : ");
+    scanf(" %[^\n]",baru->nama);
+
+    printf("Kategori : ");
+    scanf(" %[^\n]",baru->kategori);
+
+    printf("Stok : ");
+    scanf("%d",&baru->stok);
+
+    printf("Harga Sewa/Jam : ");
+    scanf("%f",&baru->harga);
+
+    baru->next = NULL;
+
+    if(head == NULL)
+    {
+        head = baru;
+    }
+    else
+    {
+        Alat *temp = head;
+
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = baru;
+    }
+
+    root = insertBST(
+        root,
+        baru->id,
+        baru->nama
+    );
+
+    insertHash(baru->id);
+
+    printf("\nData berhasil ditambahkan!\n");
+}
+
+void tampilAlat()
+{
+    Alat *temp = head;
+
+    if(temp == NULL)
+    {
+        printf("Data kosong!\n");
+        return;
+    }
+
+    while(temp != NULL)
+    {
+        printf("\n====================\n");
+        printf("ID       : %d\n",temp->id);
+        printf("Nama     : %s\n",temp->nama);
+        printf("Kategori : %s\n",temp->kategori);
+        printf("Stok     : %d\n",temp->stok);
+        printf("Harga    : %.2f\n",temp->harga);
+
+        temp = temp->next;
+    }
+}
+
+Alat* cariAlatID(int id)
+{
+    Alat *temp = head;
+
+    while(temp != NULL)
+    {
+        if(temp->id == id)
+        {
+            return temp;
+        }
+
+        temp = temp->next;
+    }
+
+    return NULL;
+}
+
+void updateAlat()
+{
+    int id;
+
+    printf("Masukkan ID : ");
+    scanf("%d",&id);
+
+    Alat *data = cariAlatID(id);
+
+    if(data == NULL)
+    {
+        printf("Data tidak ditemukan!\n");
+        return;
+    }
+
+    printf("Nama Baru : ");
+    scanf(" %[^\n]",data->nama);
+
+    printf("Kategori Baru : ");
+    scanf(" %[^\n]",data->kategori);
+
+    printf("Stok Baru : ");
+    scanf("%d",&data->stok);
+
+    printf("Harga Baru : ");
+    scanf("%f",&data->harga);
+
+    printf("Data berhasil diperbarui!\n");
+}
+
+void hapusAlat()
+{
+    int id;
+
+    printf("Masukkan ID : ");
+    scanf("%d",&id);
+
+    Alat *temp = head;
+    Alat *prev = NULL;
+
+    while(temp != NULL && temp->id != id)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if(temp == NULL)
+    {
+        printf("Data tidak ditemukan!\n");
+        return;
+    }
+
+    if(prev == NULL)
+    {
+        head = temp->next;
+    }
+    else
+    {
+        prev->next = temp->next;
+    }
+
+    free(temp);
+
+    printf("Data berhasil dihapus!\n");
+}
