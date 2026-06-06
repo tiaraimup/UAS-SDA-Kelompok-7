eda355c6824f61f162c7dd52745776ec8536ef89
auzan-valy-hafidza <auzanvaly@mhs.usk.ac.id>
2026-06-06 23:27:57 +0700

    Update alat.h

--- alat.h
+++ alat.h
@@ -1,1 +1,26 @@
+#ifndef ALAT_H
+#define ALAT_H
 
+typedef struct Alat{
+    int id;
+    char nama[50];
+    char kategori[50];
+    int stok;
+    float harga;
+
+    struct Alat *next;
+}Alat;
+
+extern Alat *head;
+
+void tambahAlat();
+void tampilAlat();
+void updateAlat();
+void hapusAlat();
+
+Alat* cariAlatID(int id);
+
+void bubbleSortNama();
+void selectionSortStok();
+
+#endif
